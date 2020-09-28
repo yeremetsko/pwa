6238d9d94ff1038be88a6b9d2a48fbce7c9da74d
yeremetsko <71814008+yeremetsko@users.noreply.github.com>
2020-09-28 15:41:32 +0300

    Add files via upload

---
+++ precache-manifest.9064483e9c79e523a1deb4ef71c4a7fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad732cb17b0136dfdf7",
+    "url": "/pwa/css/app.14d96648.css"
+  },
+  {
+    "revision": "e20021ed3d66cbfad326",
+    "url": "/pwa/css/chunk-62b315f4.d3278391.css"
+  },
+  {
+    "revision": "903701d1030c526901bf",
+    "url": "/pwa/css/chunk-bd8e6dba.d4738cf5.css"
+  },
+  {
+    "revision": "880f90873d4f83f0c1bf",
+    "url": "/pwa/css/chunk-vendors.8edd3848.css"
+  },
+  {
+    "revision": "09da6009ad3ad509d5ce8417e9e8913c",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "9ad732cb17b0136dfdf7",
+    "url": "/pwa/js/app.dcc8ecba.js"
+  },
+  {
+    "revision": "e20021ed3d66cbfad326",
+    "url": "/pwa/js/chunk-62b315f4.9e6cf352.js"
+  },
+  {
+    "revision": "903701d1030c526901bf",
+    "url": "/pwa/js/chunk-bd8e6dba.671c8b97.js"
+  },
+  {
+    "revision": "880f90873d4f83f0c1bf",
+    "url": "/pwa/js/chunk-vendors.7a83645a.js"
+  },
+  {
+    "revision": "2a161e29fb2f237e513b7f8f02700734",
+    "url": "/pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa/robots.txt"
+  }
+]);
